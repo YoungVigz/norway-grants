7d1a00f7349871325b5bd95e16098c7b8d455b7e
Karybers <nieznanyzero@interia.pl>
2021-03-23 13:29:53 +0100

    update

--- src/components/Main/text.js
+++ src/components/Main/text.js
@@ -1,6 +1,12 @@
-import pront from './popOut_Symbols/pront.png'
 import atom from './popOut_Symbols/atom.svg'
 import potka from './popOut_Symbols/potka.svg'
+
+import pront from './popOut_Symbols/pront.png'
+import prad from './popOut_Symbols/prad.svg'
+
+import torba from './popOut_Symbols/torba.svg'
+
+import maski from './popOut_Symbols/maski.svg'
 
 export const text = [
     {
@@ -14,7 +20,7 @@ export const text = [
         itemId: "arrow_fan",
         title: "Environmental protection and green technology",
         text: "The European energy sector is currently facing the challenge of moving from the use of fossil fuels and high energy consumption to an increased use of renewable energy and high energy efficiency. This move is fundamental for reducing greenhouse gas emissions. Otherwise, Europe will not be able to meet its commitments to reduce emissions. Iceland, Liechtenstein and Norway are committed to international efforts to tackle climate change, a commitment shared with the European Union. The EEA and Norway Grants will fund programmes focusing on renewable energy, energy security and energy efficiency in line with the Energy Union objectives and the Europe 2020 strategy targets of 20% reduction of greenhouse gas emission, 20% of energy from renewables and 20% increase in energy efficiency.",
-        symbol1: (<img src={pront}/>),
+        symbol1: (<img src={prad}/>),
         symbol2: (<img src={pront}/>),    
     },
     {
@@ -29,13 +35,13 @@ export const text = [
         title: "Social and regional development",
         text: "According to the EU, people are at risk of living in poverty if their income falls below 60% of median income in the country where they live. In 2011, more than 119 million people – 24% of the EU population – were at risk of poverty. The Europe 2020 strategy aims to reduce this number by 20 million by 2020. Special attention is paid to measures aimed at those running the highest risk of poverty and social exclusion: the unemployed and low-skilled, non-EU migrants and single parent families. There is an urgent need to address youth unemployment (23% in EU countries in 2013) and the unemployment of cultural and ethnic minorities and non-EU migrants. The EEA and Norway Grants target the challenges that the most vulnerable regions in the beneficiary countries are currently facing. The Grants do this by providing support to help address poverty at the root causes: lack of job prospects, lack of job capacity and problems leading to social exclusion. The support aims to empower communities to develop innovative, inclusive and bottom-up approaches to involve people in the development of their community and create better prospects for the disadvantaged. It also aims to stimulate knowledge exchange between municipalities through networking and policy exchanges. The Europe 2020 strategy and the EU’s cohesion policy take account of the crucial role Europe’s cities and regions play for future smart, sustainable and inclusive growth. Addressing poverty is a key priority in this strategy.",
         symbol1: (<img src={pront}/>),
-        symbol2: (<img src={pront}/>),
+        symbol2: (<img src={torba}/>),
     },
     {
         itemId: "arrow_cinema",
         title: "Cultural heritage",
         text: "The EEA and Norway Grants aim at broadening people’s access to culture, at promoting cultural diversity, and at strengthening intercultural dialogue. The importance of media literacy, intercultural skills and other democratic competences is underlined in the Secretary General of the Council of Europe report “State of democracy, human rights and rule of law in Europe. The EEA and Norway Grants stimulate the safeguarding of cultural heritage, cultural entrepreneurship and cultural cooperation because of the sector’s role in local and regional development, capacity development and social inclusion. The Grants recognise the sector’s potential as a driver of economic and social innovation in and outside of the cultural and creative sectors. They acknowledge that the cultural and creative sectors also play a role in stimulating social inclusion and combatting discrimination; they help forge solidarity between people and strengthen democracy. Across Europe there is a growing concern related to radicalism and violent extremism. Culture plays a role in counterbalancing this due to its role in stimulating greater mutual understanding.",
-        symbol1: (<img src={pront}/>),
+        symbol1: (<img src={maski}/>),
         symbol2: (<img src={pront}/>),
     },
 ]
